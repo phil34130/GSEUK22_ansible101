336805d705e9809769ce90da727394e309d3807a
phil34130 <33055686+phil34130@users.noreply.github.com>
2025-11-22 16:40:58 +0100

    python job

--- files/python_jcl.py
+++ files/python_jcl.py
@@ -1,17 +1,11 @@
-# Submit  Job
-
-    from zoautil_py import jobs
-
-
-    jcl_file = "/z/z04683/python/python.jcl"
-
-    job_id = jobs.submit(jcl_file, is_unix=True)
-
-
-    # Retrieve output
-
-    job_dds: list[dict] = jobs.list_dds(job_id=job_id)
-
-    raw_data: list[str] = job_dds["SYSUT2"].split("\n")
-
-    return raw_data
+#!/bin/env python                             
+import platform                               
+print("machine",platform.machine())           
+print("platform",platform.platform())         
+print("arch ",platform.architecture())        
+print("process ",platform.processor())        
+print("compiler",platform.python_compiler())  
+print("system ",platform.system())                                                      
+from zoautil_py import jobs                    
+from zoautil_py import mvscmd, datasets        
+print(jobs.fetch_multiple())  
---
+++ files/python_jcl_submit.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+from zoautil_py.jobs import submit, read_output, list_dds
+from zoautil_py.datasets import read
+from time import sleep
+
+def printjob(jobid):
+ stepds = list_dds(jobid)
+ for item in stepds:
+  print(read_output(jobid, item["step_name"], item["dd_name"]))
+
+def subsd(fname):
+
+  print(120 * '*')
+  print(read(fname))
+  print(120 * '*')
+
+  joppie = submit(fname, wait=True, timeout=20)
+
+  print("Job name:" , joppie.name, " Job ID:" , joppie.job_id, " Job owner:", joppie.owner)
+  print(120 * '*')
+
+  i=1
+  while joppie.status == 'AC':
+    sleep(0.1)
+    print('\r Running job', i* '.',"end =", flush=True)
+  i+=1
+  joppie.refresh()
+
+  printjob(joppie.job_id)
+  print(120 * '*')
+
+subsd('Z04683.ANSB.JCL(HELLO)')
